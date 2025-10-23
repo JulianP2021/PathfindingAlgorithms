52daf83a4cad657eb12173afd57a96920fef8840
Julian Scharf <julian@scharf.am>
2025-10-23 22:41:45 +0200

    Update app title in header
    Changed the header text from 'Pathfinding Visualizer by Julian' to 'Pathfinding Visualizer - Julian' for consistency in branding.

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
 
         <main class="main">
             <header class="title-card">
-                <h1>Pathfinding Visualizer by Julian</h1>
+                <h1>Pathfinding Visualizer - Julian</h1>
             </header>
 
             <section class="canvas-wrap">
